28a91ddcc35191031848484d59e5027706b8e132
luca <lucasn@al.insper.edu.br>
2017-03-31 10:08:41 -0300

    def battle

---
+++ funçao_batalha.py
@@ -0,0 +1,14 @@
+def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente):
+	while vida_jogador > 0 and vida_oponente > 0:
+		x = poder_jogador - defesa_oponente
+		if x <= 0:
+			x = 0
+		vida_oponente = vida_oponente - (x)
+		if vida_oponente <= 0:
+			return "você ganhou"
+		y = poder_oponente - defesa_jogador
+		if y <= 0:
+			y = 0
+		vida_jogador = vida_jogador - (y)
+		if vida_jogador <= 0:
+			return "você perdeu"
