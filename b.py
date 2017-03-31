3cb44df6c267159e18392f839ffcb2d35580dd9b
luigicorradi <luigicc@al.insper.edu.br>
2017-03-31 11:20:11 -0300

    importada a funcao batalha

--- b.py
+++ b.py
@@ -1,18 +1,22 @@
 import json
 import random
+import batalha
 
 with open ("inspermons.json") as wild_inspermons:
 	a = json.load(wild_inspermons)
 
 while True:
+	x = random.choice(a)
 	pergunta = input("passear ou dormir? ")
 	if pergunta == "dormir":
 		print("Até mais")
 		break
 	elif pergunta == "passear":
-		print("uma batalha acontecera aqui")
+		print("Voce ira batalhar com {0}".format(x['nome']))
+		print(batalha.battle(x['vida'],1000,1000,1000,x['defesa'],x['poder']))
+		break
 	else:
 		print("ERRO, reposta invalida, tente novamente")
 		continue 
-	
-	
+
+
