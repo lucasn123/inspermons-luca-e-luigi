fef6419249906325639b6c2056070b23301a00aa
luigicorradi <luigicc@al.insper.edu.br>
2017-04-11 18:08:32 -0300

    melhorias nas mensagem de texto

--- b.py
+++ b.py
@@ -1,13 +1,18 @@
 import json
 import random
-import batalha
-import pet
+import batalha #funcao batalha
+import pet #programa que permite a escolha do seu inspermon inicial
+import time
+
 with open ("inspermons.json") as wild_inspermons:
 	a = json.load(wild_inspermons)
+
 vidaj = pet.pet['vida']
 poderj = pet.pet['poder']
 defesaj = pet.pet['defesa']
-fugirLista = [1,2,3,4,5]
+fugirLista = [1,2,3,4,5] #lista que define a chance de fugir, no caso 1/5
+
+#codigo basico
 while True:
 	x = random.choice(a)
 	pergunta = input("passear ou dormir? ")
@@ -15,21 +20,39 @@ while True:
 		print("Até mais")
 		break
 	elif pergunta == "passear":
-		print("um {0} muito louco brotou do nada".format(x['nome']))
-		resposta = input('Você deseja batalhar ou fugir? ')  
+		print("um {0} muito louco brotou do nada\n vida: {1}\n poder: {2}\n defesa: {3}".format(x['nome'], x['vida'],x['poder'],x['defesa']))
+		time.sleep(2)
+		resposta = input('Você deseja batalhar ou fugir? ') #opcao de fuga
 		if resposta == 'fugir':
 			z = random.choice(fugirLista)
 			if z == 1:
 				print('Você fugiu')
 				continue
+
 			else:
 				print('Não foi possível fugir nesse momento')
-				print('A batalha continua')
-				print(batalha.battleY(x['vida'],vidaj,poderj,defesaj,x['defesa'],x['poder']))
+				print('A batalha continua e voce perdeu seu round de ataque')
+				resultado = batalha.battleY(x['vida'],vidaj,poderj,defesaj,x['defesa'],x['poder'], x['nome'])
+				if resultado == "você perdeu":
+					print('voce morreu, seu noob')
+					time.sleep(3)
+					print('GAME OVER')
+					break
+				else:
+					print("você ganhou")
+
 
 		elif resposta == 'batalhar':
-			print(batalha.battle(x['vida'],vidaj,poderj,defesaj,x['defesa'],x['poder']))
-	
+			resultado = batalha.battle(x['vida'],vidaj,poderj,defesaj,x['defesa'],x['poder'],x['nome'])
+			if resultado == "você perdeu":
+				print('voce morreu, seu noob')
+				time.sleep(3)
+				print('GAME OVER')
+				break
+			else:
+				print("você ganhou")
+		else:
+			print("ERRO, reposta invalida, tente novamente")
 
 	else:
 		print("ERRO, reposta invalida, tente novamente")
