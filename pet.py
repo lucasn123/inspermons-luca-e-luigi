3eda2113daed87298a7778dd1f3e5d7d23be80e0
luigicorradi <luigicc@al.insper.edu.br>
2017-04-11 18:11:13 -0300

    melhorias nas mensagens de texto

--- pet.py
+++ pet.py
@@ -2,14 +2,20 @@ import json
 
 with open ("inspermons.json") as wild_inspermons:
 	a = json.load(wild_inspermons)
-
-print("esses são os inspèrmons disponíveis xamando, picaxu e pidjet")
-b = input("escolha um dos inspèrmons disponíveis: ")
-if b =="picaxu":
-	pet = a[0]
-elif b == "xamando":
-	pet = a[1]
-elif b == "pidjet":
-	pet = a[2]
+while True:
+	print("esses são os inspèrmons disponíveis: xamando, picaxu e pidjet")
+	b = input("escolha um dos inspèrmons disponíveis: ")
+	if b =="picaxu":
+		pet = a[0]
+		break
+	elif b == "xamando":
+		pet = a[1]
+		break
+	elif b == "pidjet":
+		pet = a[2]
+		break
+	else:
+		print("ERRO, reposta invalida, tente novamente")
+		continue
 
 print("esses são os status do seu novo inspèrmon: \n nome: {0} \n vida: {1} \n poder: {2} \n defesa: {3} ". format(pet['nome'],pet['vida'],pet['poder'],pet['defesa']))
