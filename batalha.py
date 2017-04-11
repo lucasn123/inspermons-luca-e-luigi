60f7bc7fbca95be0446d40128f201e024f50bb2d
luigicorradi <luigicc@al.insper.edu.br>
2017-04-11 18:09:32 -0300

    mostrando os rounds na funcao batalha

--- batalha.py
+++ batalha.py
@@ -1,9 +1,12 @@
-def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente):
+import time
+def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente,nome_oponente):
 	while vida_jogador > 0 and vida_oponente > 0:
 		x = poder_jogador - defesa_oponente
 		if x <= 0:
 			x = 0
 		vida_oponente = vida_oponente - (x)
+		print("Voce atacou {0}\n vida restante do oponente: {1}". format(nome_oponente,vida_oponente))
+		time.sleep(1)
 		if vida_oponente <= 0:
 			return "você ganhou"
           
@@ -11,21 +14,29 @@ def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_opon
 		if y <= 0:
 			y = 0
 		vida_jogador = vida_jogador - (y)
+		print("{0} atacou voce\n  sua vida restante: {1}". format(nome_oponente,vida_jogador))
+		time.sleep(1)
 		if vida_jogador <= 0:
 			return "você perdeu"
+			
 
-def battleY(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente):
+def battleY(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente, nome_oponente):
 	while vida_jogador > 0 and vida_oponente > 0:	
 		y = poder_oponente - defesa_jogador
 		if y <= 0:
 			y = 0
 		vida_jogador = vida_jogador - (y)
+		print("{0} atacou voce\n  sua vida restante: {1}". format(nome_oponente,vida_jogador))
+		time.sleep(1)
+
 		if vida_jogador <= 0:
 			return "você perdeu"
 		x = poder_jogador - defesa_oponente
 		if x <= 0:
 			x = 0
 		vida_oponente = vida_oponente - (x)
+		print("Voce atacou {0}\n vida restante do oponente: {1}". format(nome_oponente,vida_oponente))
+		time.sleep(1)
 		if vida_oponente <= 0:
 			return "você ganhou"
 					
