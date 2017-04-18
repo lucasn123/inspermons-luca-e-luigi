d9e92e9851f9641cfcc6012329ffa929f64264b9
FernandoMauad <fernandomauad97@gmail.com>
2017-04-18 15:23:52 -0300

    adicionei a funcao sorte na batalha, podendo dar critical hit

--- batalha.py
+++ batalha.py
@@ -1,7 +1,23 @@
 import time
+import random
+sorteLista = [0,1,2,3,4,5,6]
+
+
 def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente,nome_oponente):
+    
 	while vida_jogador > 0 and vida_oponente > 0:
-		x = poder_jogador - defesa_oponente
+		y = poder_oponente - defesa_jogador
+		if y <= 0:
+			y = 0
+		vida_jogador = vida_jogador - (y)
+		if vida_jogador <= 0:
+			return "você perdeu"
+		resposta2 = random.choice(sorteLista)
+		if resposta2 == 6:
+			print('Critical Hit !!')
+			x = poder_jogador*2 - defesa_oponente
+		else:
+			x = poder_jogador - defesa_oponente
 		if x <= 0:
 			x = 0
 		vida_oponente = vida_oponente - (x)
@@ -9,7 +25,6 @@ def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_opon
 		time.sleep(1)
 		if vida_oponente <= 0:
 			return "você ganhou"
-          
 		y = poder_oponente - defesa_jogador
 		if y <= 0:
 			y = 0
@@ -18,6 +33,12 @@ def battle(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_opon
 		time.sleep(1)
 		if vida_jogador <= 0:
 			return "você perdeu"
+            
+        
+          
+
+
+  
 			
 
 def battleY(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_oponente, poder_oponente, nome_oponente):
@@ -31,7 +52,12 @@ def battleY(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_opo
 
 		if vida_jogador <= 0:
 			return "você perdeu"
-		x = poder_jogador - defesa_oponente
+		resposta2 = random.choice(sorteLista)
+		if resposta2 == 6:
+			print('Critical Hit !!')
+			x = poder_jogador*2 - defesa_oponente
+		else:
+			x = poder_jogador - defesa_oponente
 		if x <= 0:
 			x = 0
 		vida_oponente = vida_oponente - (x)
@@ -39,4 +65,7 @@ def battleY(vida_oponente,vida_jogador, poder_jogador,defesa_jogador, defesa_opo
 		time.sleep(1)
 		if vida_oponente <= 0:
 			return "você ganhou"
+            
+
+
 					
